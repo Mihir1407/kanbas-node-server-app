94e4ce4d9bececb185d8f84f26727747e9803582
Mihir Chitre <chitre.m@northeastern.edu>
2023-12-02 00:30:04 -0500

    Worked on assignment 6

--- app.js
+++ app.js
@@ -7,17 +7,35 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
 const isDevelopment = process.env.NODE_ENV === "development";
 const app = express();
 app.use(
-    cors({
-      credentials: true,
-      origin: isDevelopment
-        ? process.env.FRONTEND_URL 
-        : "https://statuesque-kringle-a101ff.netlify.app"
-    })
-  );
+  cors({
+    credentials: true,
+    origin: isDevelopment
+      ? process.env.FRONTEND_URL
+      : "https://statuesque-kringle-a101ff.netlify.app"
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
+UserRoutes(app);
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
